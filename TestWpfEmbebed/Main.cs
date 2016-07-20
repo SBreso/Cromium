8e771e29817e88aaf4b6d5105e6731535713185e
Sbreso <sbresograncha@gmail.com>
2016-07-20 17:11:29 +0200

    Agregar archivos de proyecto.

---
+++ TestWpfEmbebed/Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.Integration;
+
+namespace TestWpfEmbebed
+{
+    sealed class MainApp
+    {
+        [STAThread()]
+        static internal void Main()
+        {
+            try
+            {
+                MainWindow win = new MainWindow();
+                ElementHost.EnableModelessKeyboardInterop(win);
+                win.Show();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
